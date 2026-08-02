859b6f34e7ca5dce83c7a60b380236f0bb0d569f
rion-77-test <testmailrion77@gmail.com>
2026-08-02 13:00:47 +0600

    index page templating

--- Project/src/main.tsx
+++ Project/src/main.tsx
@@ -1,6 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+// Css and Js
+// import './assets/css/app.css'
+// import './assets/css/app-dark.css'
+// import './assets/css/iconly.css'
+import './assets/compiled/css/app.css'
+import './assets/compiled/css/app-dark.css'
+import './assets/compiled/css/iconly.css'
+
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
