13030c0b9d318aac42afebe4f7e035e6c8608aa4
rion-77-test <testmailrion77@gmail.com>
2026-08-08 12:55:56 +0600

    light mode & dark mode works partially

--- Project/public/assets/static/js/components/dark.js
+++ Project/public/assets/static/js/components/dark.js
@@ -1,13 +1,13 @@
 
 const THEME_KEY = "theme"
 
-function toggleDarkTheme() {
-  setTheme(
-    document.documentElement.getAttribute("data-bs-theme") === 'dark'
-      ? "light"
-      : "dark"
-  )
-}
+// function toggleDarkTheme() {
+//   setTheme(
+//     document.documentElement.getAttribute("data-bs-theme") === 'dark'
+//       ? "light"
+//       : "dark"
+//   )
+// }
 
 /**
  * Set theme for mazer
@@ -26,7 +26,7 @@ function setTheme(theme, persist = false) {
 /**
  * Init theme from setTheme()
  */
-function initTheme() {
+/* function initTheme() {
   //If the user manually set a theme, we'll load that
   const storedTheme = localStorage.getItem(THEME_KEY)
   if (storedTheme) {
@@ -45,19 +45,46 @@ function initTheme() {
     setTheme(e.matches ? "dark" : "light", true)
   )
   return setTheme(mediaQuery.matches ? "dark" : "light", true)
-}
+} */
 
-window.addEventListener('DOMContentLoaded', () => {
-  const toggler = document.getElementById("toggle-dark")
-  const theme = localStorage.getItem(THEME_KEY)
+// window.addEventListener('DOMContentLoaded', () => {
+//   const toggler = document.getElementById("toggle-dark")
+//   const theme = localStorage.getItem(THEME_KEY)
 
-  if(toggler) {
-    toggler.checked = theme === "dark"
+//   if(toggler) {
+//     toggler.checked = theme === "dark"
     
-    toggler.addEventListener("input", (e) => {
-      setTheme(e.target.checked ? "dark" : "light", true)
-    })
+//     toggler.addEventListener("input", (e) => {
+//       setTheme(e.target.checked ? "dark" : "light", true)
+//     })
+//   }
+
+// });
+window.addEventListener('click', (e) => {
+  const toggler = e.target.closest("#toggle-dark")
+
+  if(!toggler) return;
+
+  // Selects html tag and gets attribute
+  const htmlTag = document.documentElement;
+  const currentTheme = htmlTag.getAttribute("data-bs-theme");
+  console.log(currentTheme);
+  
+  if(currentTheme === "dark") {
+    htmlTag.setAttribute("data-bs-theme", "light")
+  } else {
+    htmlTag.setAttribute("data-bs-theme", "dark")
   }
+  // const theme = localStorage.getItem(THEME_KEY);
+  // console.log(theme);
+
+  // if(toggler) {
+  //   toggler.checked = theme === "dark"
+    
+  //   toggler.addEventListener("input", (e) => {
+  //     setTheme(e.target.checked ? "dark" : "light", true)
+  //   })
+  // }
 
 });
 
